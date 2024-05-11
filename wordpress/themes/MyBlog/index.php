40ca6d5da7ac9f6bb49c9e0b7484254ccc5e7226
hokuto-kato <hokuto15@gmail.com>
2024-05-11 19:14:56 +0900

    update: テーマindex.phpをWordPressループにリファクタリング、SCSSのヘッダースタイルを微調整
    - ページ内の投稿表示をWordPressループに対応させるため、index.phpをリファクタリングしました
    - 投稿が存在しない場合のメッセージを追加しました
    - SCSSでヘッダースタイルのセレクタを微調整しました
    - 必須のWordPressテンプレートタグ（wp_head、wp_footer）を追加しました

--- wordpress/themes/MyBlog/index.php
+++ wordpress/themes/MyBlog/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -13,19 +14,23 @@
   <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
   <!-- Core theme CSS (includes Bootstrap)-->
-	<?php if ( WP_DEBUG ) {
-		$root    = 'http://localhost:5173';
-		$css_ext = 'scss';
-		$js_ext  = 'ts';
-		echo '<script type="module" src="http://localhost:5173/@vite/client"></script>';
-	} else {
-		$root    = get_template_directory_uri();
-		$css_ext = 'css';
-		$js_ext  = 'js';
-	} ?>
+  <?php if (WP_DEBUG) {
+    $root    = 'http://localhost:5173';
+    $css_ext = 'scss';
+    $js_ext  = 'ts';
+    echo '<script type="module" src="http://localhost:5173/@vite/client"></script>';
+  } else {
+    $root    = get_template_directory_uri();
+    $css_ext = 'css';
+    $js_ext  = 'js';
+  } ?>
+
   <link rel="stylesheet" href="<?php echo $root; ?>/assets/style/style.<?php echo $css_ext; ?>">
   <script src="<?php echo $root; ?>/assets/js/script.<?php echo $js_ext; ?>" type="module"></script>
+  <!--必須テンプレートタグ-->
+  <?php wp_head(); ?>
 </head>
+
 <body>
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
@@ -37,10 +42,18 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ms-auto py-4 py-lg-0">
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
+        <li class="nav-item">
+          <a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a>
+        </li>
       </ul>
     </div>
   </div>
@@ -51,7 +64,7 @@
     <div class="row gx-4 gx-lg-5 justify-content-center">
       <div class="col-md-10 col-lg-8 col-xl-7">
         <div class="site-heading">
-          <h1>Clean Blog <?php echo date( "Y" ) ?></h1>
+          <h1>Clean Blog <?php echo date("Y") ?></h1>
           <span class="subheading">A Blog Theme by Start Bootstrap</span>
         </div>
       </div>
@@ -63,63 +76,33 @@
   <div class="row gx-4 gx-lg-5 justify-content-center">
     <div class="col-md-10 col-lg-8 col-xl-7">
       <!-- Post preview-->
-      <div class="post-preview">
-        <a href="post.html">
-          <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-          <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
-        </a>
-        <p class="post-meta">
-          Posted by
-          <a href="#!">Start Bootstrap</a>
-          on September 24, 2023
-        </p>
-      </div>
-      <!-- Divider-->
-      <hr class="my-4" />
-      <!-- Post preview-->
-      <div class="post-preview">
-        <a href="post.html">
-          <h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.</h2>
-        </a>
-        <p class="post-meta">
-          Posted by
-          <a href="#!">Start Bootstrap</a>
-          on September 18, 2023
-        </p>
-      </div>
-      <!-- Divider-->
-      <hr class="my-4" />
-      <!-- Post preview-->
-      <div class="post-preview">
-        <a href="post.html">
-          <h2 class="post-title">Science has not yet mastered prophecy</h2>
-          <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.</h3>
-        </a>
-        <p class="post-meta">
-          Posted by
-          <a href="#!">Start Bootstrap</a>
-          on August 24, 2023
-        </p>
-      </div>
-      <!-- Divider-->
-      <hr class="my-4" />
-      <!-- Post preview-->
-      <div class="post-preview">
-        <a href="post.html">
-          <h2 class="post-title">Failure is not an option</h2>
-          <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future generations.</h3>
-        </a>
-        <p class="post-meta">
-          Posted by
-          <a href="#!">Start Bootstrap</a>
-          on July 8, 2023
-        </p>
-      </div>
-      <!-- Divider-->
-      <hr class="my-4" />
-      <!-- Pager-->
-      <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a>
-      </div>
+      <?php if (have_posts()) : ?>
+        <?php while (have_posts()) : ?>
+          <div class="post-preview">
+            <a href="post.html">
+              <h2 class="post-title">
+                <?php the_post() ?>
+                <?php the_title() ?>
+              </h2>
+              <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+            </a>
+            <p class="post-meta">
+              Posted by
+              <a href="#!"><?php the_author() ?></a><br>
+              on <?php the_time('F j, Y') ?>
+            </p>
+          </div>
+          <!-- Divider-->
+          <hr class="my-4" />
+        <?php endwhile ?>
+
+        <!-- Pager-->
+        <div class="d-flex justify-content-end mb-4">
+          <a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a>
+        </div>
+      <?php else : ?>
+        <p>Article Not Found</p>
+      <?php endif ?>
     </div>
   </div>
 </div>
@@ -131,26 +114,26 @@
         <ul class="list-inline text-center">
           <li class="list-inline-item">
             <a href="#!">
-              <span class="fa-stack fa-lg">
-                <i class="fas fa-circle fa-stack-2x"></i>
-                <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-              </span>
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
             </a>
           </li>
           <li class="list-inline-item">
             <a href="#!">
-              <span class="fa-stack fa-lg">
-                <i class="fas fa-circle fa-stack-2x"></i>
-                <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-              </span>
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
             </a>
           </li>
           <li class="list-inline-item">
             <a href="#!">
-              <span class="fa-stack fa-lg">
-                <i class="fas fa-circle fa-stack-2x"></i>
-                <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-              </span>
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                </span>
             </a>
           </li>
         </ul>
@@ -161,5 +144,9 @@
 </footer>
 <!-- Bootstrap core JS-->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<!--必須テンプレートタグ-->
+<?php wp_footer(); ?>
 </body>
+
 </html>
