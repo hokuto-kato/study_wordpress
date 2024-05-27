789c6b596f1016f93e438c7f777561d877a46e9a
hokuto-kato <hokuto15@gmail.com>
2024-05-27 16:41:13 +0900

    feat: 新規にアーカイブページを追加した
    新規にアーカイブページ(archive.php)を作成し、WordPressのカテゴリーページで使用されるようにしました。
    このページからは、特定のカテゴリに所属する記事が一覧表示されます。

---
+++ wordpress/themes/MyBlog/archive.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+  <?php get_header(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <?php get_template_part('includes/header'); ?>
+  <!-- Page Header-->
+  <header class="masthead -home">
+    <div class="container position-relative px-4 px-lg-5">
+      <div class="row gx-4 gx-lg-5 justify-content-center">
+        <div class="col-md-10 col-lg-8 col-xl-7">
+          <div class="site-heading">
+            <h1>Category</h1>
+            <span class="subheading"><?php single_cat_title(); ?></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Main Content-->
+  <div class="container px-4 px-lg-5">
+    <div class="row gx-4 gx-lg-5 justify-content-center">
+      <div class="col-md-10 col-lg-8 col-xl-7">
+        <!-- Post preview-->
+        <?php if (have_posts()) : ?>
+          <?php while (have_posts()) : the_post() ?>
+            <div class="post-preview">
+              <a href="<?php the_permalink() ?>">
+                <h2 class="post-title">
+                  <?php the_title() ?>
+                </h2>
+                <h3 class="post-subtitle">
+                  <?php the_excerpt() ?>
+                </h3>
+              </a>
+              <p class="post-meta">
+                Posted by
+                <?php the_author() ?> on <?php the_time('F j, Y') ?>
+              </p>
+            </div>
+            <!-- Divider-->
+            <hr class="my-4" />
+          <?php endwhile ?>
+          <!-- Pager-->
+          <div class="d-flex justify-content-end mb-4">
+            <a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a>
+          </div>
+        <?php else : ?>
+          <p>Article Not Found</p>
+        <?php endif ?>
+      </div>
+    </div>
+  </div>
+  <?php get_template_part("includes/footer.php") ?>
+  <?php get_footer() ?>
+</body>
+
+</html>
