b1de0651368eb0f3ff9de1fd4d31c3cabb5a434c
hokuto-kato <hokuto15@gmail.com>
2024-06-01 23:33:03 +0900

    update: ヘッダーのメニューリンクを動的に生成するように改善
    ヘッダーのメニューリンクを hard code から WordPress のメニュー機能を利用する形式に更新しました。これにより、メニューのアイテムを管理画面から設定可能になります。また、Bootstrap 用のカスタムウォーカークラスも追加しました。

--- wordpress/themes/MyBlog/includes/header.php
+++ wordpress/themes/MyBlog/includes/header.php
@@ -1,23 +1,26 @@
 <!-- Navigation-->
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
   <div class="container px-4 px-lg-5">
-    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+    <a class="navbar-brand" href="/">Start Bootstrap</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       Menu
       <i class="fas fa-bars"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav ms-auto py-4 py-lg-0">
-        <li class="nav-item">
-          <a class="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link px-lg-3 py-3 py-lg-4" href="/about/">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link px-lg-3 py-3 py-lg-4" href="/contact/">Contact</a>
-        </li>
-      </ul>
+      <?php
+      // メニューが設定されているか確認し、設定されていれば表示する
+      if (has_nav_menu('global-nav')) {
+        wp_nav_menu(array(
+          'theme_location' => 'global-nav',
+          'menu_class'     => 'navbar-nav ms-auto py-4 py-lg-0',
+          'container'      => false,
+          'depth'          => 2,
+          'fallback_cb'    => false,
+          'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+          'walker'         => new WP_Bootstrap_Navwalker() // Bootstrap用のカスタムウォーカークラスが必要な場合
+        ));
+      }
+      ?>
     </div>
   </div>
 </nav>
